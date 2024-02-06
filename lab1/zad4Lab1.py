cebc32e40f82802f77b88374857b31871011a1bb
Денис Талышкин <denis.talyshkin@mail.ru>
2024-02-06 17:26:40 +0300

    2

--- lab1/zad4Lab1.py
+++ lab1/zad4Lab1.py
@@ -1,8 +1,12 @@
 try:
     chislo = int(input('Введите двоичное число: '));
+    a = 0;
 except ValueError:
     print('fef');
 finally:
-    while(chislo>=1):
-        numb = chislo%2;
-        print();dwd
+    while (chislo >1):
+        if(chislo%2==1):
+            a+=1;
+        else:
+            break;
+print(a);
