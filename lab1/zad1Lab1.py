fdee8dd6317a6b47a4707e2b0552bbaf200f150f
Денис Талышкин <denis.talyshkin@mail.ru>
2024-02-06 18:06:46 +0300

    1

--- lab1/zad1Lab1.py
+++ lab1/zad1Lab1.py
@@ -2,9 +2,8 @@
 try:
     slovo = input('Введите слово: ');
     print('Дано слово: ', slovo);
+    bukva = input('Введите букву: ');
+    print(bukva, slovo.index(bukva)+1);
 except ValueError:
     print('Не то');
-finally:
-    bukva = input('Введите букву: ');
-print(bukva, slovo.index(bukva)+1);
   #print(slovo.index('c'))
