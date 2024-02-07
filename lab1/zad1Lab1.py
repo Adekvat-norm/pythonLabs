b915e535a935bd70f50a7fd366ad977bd524b8c0
Денис Талышкин <denis.talyshkin@mail.ru>
2024-02-07 18:00:02 +0300

    1

--- lab1/zad1Lab1.py
+++ lab1/zad1Lab1.py
@@ -1,9 +1,19 @@
 
 try:
-    slovo = input('Введите слово: ');
-    print('Дано слово: ', slovo);
-    bukva = input('Введите букву: ');
-    print(bukva, slovo.index(bukva)+1);
+    slovo = input('Введите слово: ')
+    if (slovo == ''):
+        print('Это пустое слово...')
+    else:
+        print('Дано слово: ', slovo)
+        bukva = input('Введите букву: ')
+        if bukva in slovo:
+            print('Индексы буквы "{}" в слове "{}":'.format(bukva, slovo))
+            index = 0
+            for char in slovo:
+                if char == bukva:
+                    print(index)
+                index += 1
+        else:
+            print('Буква "{}" не найдена в слове "{}"'.format(bukva, slovo))
 except ValueError:
-    print('Не то');
-  #print(slovo.index('c'))
+    print('Не то')
