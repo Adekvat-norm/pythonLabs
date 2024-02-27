fb45589cb44719c40ddcf6f8a2bea27f9ffaf4b0
Денис Талышкин <denis.talyshkin@mail.ru>
2024-02-27 06:35:45 +0300

    2

---
+++ lab2/zad2Lab2.py
@@ -0,0 +1,4 @@
+a=[];
+spisok = [a.append(x) for x in input('Введите элементы списка: ')];
+a.sort();
+print(a[-1]);
---
+++ lab2/zad3Lab2.py
@@ -0,0 +1,9 @@
+a=[];
+spisok = [a.append(x) for x in input('Введите элементы в списке: ')];
+index=0;
+bukva=a[index];
+if(a.count(bukva)>=2):
+    print(a[index]);
+    index+=1;
+else:
+    index+=1;
