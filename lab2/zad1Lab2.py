8891284bb76a17a8e6389e8be830a4967f8935ef
Денис Талышкин <denis.talyshkin@mail.ru>
2024-02-27 06:13:46 +0300

    1

---
+++ lab2/zad1Lab2.py
@@ -0,0 +1,17 @@
+a = [];
+spisok = [a.append(x) for x in input('Введите элементы списка: ')];
+n = int(input('Какое действие вы хотите сделать?\n 1.Сложение\n 2.Произведение '));
+index = 0;
+if (n==1):
+    summ=0;
+    while(index<=len(a)-1):
+        summ += int(a[index]);
+        index+=1;
+        print(summ);
+if(n==2):
+    umnj=1;
+    while(index<=len(a)-1):
+        umnj *= int(a[index]);
+        index+=1;
+        print(umnj);
+        
