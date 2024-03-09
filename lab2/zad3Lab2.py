eb6237dda86de2b57c1ded17e480e96a2cdbb802
Денис Талышкин <denis.talyshkin@mail.ru>
2024-03-09 12:01:06 +0300

    3

--- lab2/zad3Lab2.py
+++ lab2/zad3Lab2.py
@@ -1,9 +1,11 @@
 a=[];
 spisok = [a.append(x) for x in input('Введите элементы в списке: ')];
 index=0;
-bukva=a[index];
-if(a.count(bukva)>=2):
-    print(a[index]);
-    index+=1;
-else:
-    index+=1;
+while(a):
+    bukva = a[index];
+    if(a.count(bukva)>=2):
+        print(a[index], a.count(bukva));
+        while bukva in a:
+            a.remove(bukva);
+    else:
+        a.remove(bukva);
