c4f167c57c934a8155cd7a274808947dbf5d8b67
Денис Талышкин <denis.talyshkin@mail.ru>
2024-03-09 13:01:20 +0300

    4

---
+++ lab2/zad4Lab2.py
@@ -0,0 +1,16 @@
+a=[];
+spisok = [a.append(x) for x in input('Введите элементы в списке: ')];
+largest = a[0];
+smallest = a[0];
+largest_index = 0;
+smallest_index = 0;
+for i, numb in enumerate(a):
+    if numb>largest:
+        largest=numb;
+        largest_index = i;
+    if numb<smallest:
+        smallest=numb;
+        smallest_index = i;
+a[largest_index], a[smallest_index] = a[smallest_index], a[largest_index];
+print(a);
+
